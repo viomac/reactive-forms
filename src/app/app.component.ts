be78118aef12a7daebd5226e8d31e526f0369324
vime <vime@sofico.be>
2020-11-23 20:08:39 -0600

    Step 2 - Associate the FormGroup model and view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-name-editor></app-name-editor>
+    <app-profile-editor></app-profile-editor>
   `,
   styleUrls: ['./app.component.css']
 })
--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -4,9 +4,19 @@ import {FormControl, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-profile-editor',
   template: `
-    <p>
-      profile-editor works!
-    </p>
+    <h2>Form group control</h2>
+    <form [formGroup]="profileForm" (ngSubmit)="onSubmit()">
+      <label>
+        First Name:
+        <input type="text" formControlName="firstName">
+      </label>
+      <label>
+        Last Name:
+        <input type="text" formControlName="lastName">
+      </label>
+
+      <button type="su bmit" [disabled]="!profileForm.valid">Submit</button>
+    </form>
   `,
   styleUrls: ['./profile-editor.component.css']
 })
@@ -17,4 +27,8 @@ export class ProfileEditorComponent {
     lastName: new FormControl('')
   });
   constructor() { }
+
+  onSubmit(): void {
+    console.warn(this.profileForm.value);
+  }
 }
